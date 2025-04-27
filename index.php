e2ee16563752b4ba06fd88b7c1b644e25e323d10
Volodymyr Kamuz <v.kamuz@gmail.com>
2025-04-27 18:00:07 +0300

    Update staging

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
-<?php
-
-header('Location: https://www.upwork.com/freelancers/~01c645635aee41c921');
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<h1>Hello, World!</h1>
+</body>
+</html>
